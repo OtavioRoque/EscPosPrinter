34a67ed0363e1a06f69c8a6ac731e63d19df12d8
Otavio <roqueotavio5@gmail.com>
2025-12-22 10:37:38 -0300

    feat: add WriteBarcode method with required constants and enums

--- EscPosPrinter/EscPosPrinter.cs
+++ EscPosPrinter/EscPosPrinter.cs
@@ -21,6 +21,27 @@ namespace EscPosPrinter
         Underline = 0x80
     }
 
+    public enum BarcodeWidth : byte
+    {
+        Narrow = 2,
+        Medium = 3,
+        Wide = 4
+    }
+
+    public enum BarcodeHeight : byte
+    {
+        Short = 60,
+        Medium = 120,
+        Tall = 180
+    }
+
+    public enum BarcodeHri : byte
+    {
+        None = 0x00,
+        Above = 0x01,
+        Below = 0x02
+    }
+
     #endregion
 
     public class EscPosPrinter
@@ -30,6 +51,12 @@ namespace EscPosPrinter
         private const byte CMD_ALIGN = 0x61;
         private const byte CMD_STYLE = 0x21;
         private const byte LF = 0x0A;
+        private const byte GS = 0x1D;
+        private const byte CMD_BARCODE_HRI = 0x48;
+        private const byte CMD_BARCODE_WIDTH = 0x77;
+        private const byte CMD_BARCODE_HEIGHT = 0x68;
+        private const byte CMD_BARCODE_PRINT = 0x6B;
+        private const byte BARCODE_CODE128 = 0x49;
 
         private readonly string _printerName;
         private readonly int _columns;
@@ -62,12 +89,31 @@ namespace EscPosPrinter
         public void WriteLeftRight(string left, string right, Style style = Style.None)
         {
             int spaces = _columns - left.Length - right.Length;
-            if (spaces < 1) 
+            if (spaces < 1)
                 spaces = 1;
 
             string line = left + new string(' ', spaces) + right;
 
             Write(line, style);
+        }
+
+        public void WriteBarcode(string content, BarcodeWidth width = BarcodeWidth.Medium, BarcodeHeight height = BarcodeHeight.Medium, BarcodeHri hri = BarcodeHri.None)
+        {
+            ApplyAlignment(Alignment.Center);
+
+            SendCommand(GS, CMD_BARCODE_HRI, (byte)hri);
+            SendCommand(GS, CMD_BARCODE_WIDTH, (byte)width);
+            SendCommand(GS, CMD_BARCODE_HEIGHT, (byte)height);
+
+            byte[] prefix = [0x7B, 0x42];
+            byte[] data = _encoding.GetBytes(content);
+            byte[] payload = prefix.Concat(data).ToArray();
+
+            SendCommand(GS, CMD_BARCODE_PRINT, BARCODE_CODE128, (byte)payload.Length);
+            SendCommand(payload);
+            SendCommand(LF);
+
+            ApplyAlignment(Alignment.Left);
         }
 
         public void WriteSplitter(char sep = '-')
