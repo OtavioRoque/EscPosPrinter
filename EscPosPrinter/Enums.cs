1db11f7025515966b63c412746bbabbcb81d5ca9
Otavio <roqueotavio5@gmail.com>
2025-12-22 13:47:12 -0300

    refactor: remove Enums class so the global using is not necesssary anymore

--- EscPosPrinter/Enums.cs
+++ EscPosPrinter/Enums.cs
@@ -1,45 +1,40 @@
-﻿global using static EscPosPrinter.Enums;
-
-namespace EscPosPrinter
+﻿namespace EscPosPrinter
 {
-    public class Enums
+    [Flags]
+    public enum Style : byte
     {
-        [Flags]
-        public enum Style : byte
-        {
-            None = 0,
-            Bold = 8,
-            DoubleHeight = 16,
-            DoubleWidth = 32,
-            Underline = 128
-        }
+        None = 0,
+        Bold = 8,
+        DoubleHeight = 16,
+        DoubleWidth = 32,
+        Underline = 128
+    }
 
-        public enum Alignment : byte
-        {
-            Left = 0,
-            Center = 1,
-            Right = 2
-        }
+    public enum Alignment : byte
+    {
+        Left = 0,
+        Center = 1,
+        Right = 2
+    }
 
-        public enum BarcodeWidth : byte
-        {
-            Narrow = 2,
-            Medium = 3,
-            Wide = 4
-        }
+    public enum BarcodeWidth : byte
+    {
+        Narrow = 2,
+        Medium = 3,
+        Wide = 4
+    }
 
-        public enum BarcodeHeight : byte
-        {
-            Short = 60,
-            Medium = 120,
-            Tall = 180
-        }
+    public enum BarcodeHeight : byte
+    {
+        Short = 60,
+        Medium = 120,
+        Tall = 180
+    }
 
-        public enum BarcodeHri : byte
-        {
-            None = 0,
-            Above = 1,
-            Below = 2
-        }
+    public enum BarcodeHri : byte
+    {
+        None = 0,
+        Above = 1,
+        Below = 2
     }
 }
